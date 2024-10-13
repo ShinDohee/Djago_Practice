f7e47cc327333fd3f5070599107c128cf47b5a3b
ShinDohee <intei0909@gmail.com>
2024-10-13 11:50:11 +0900

    Django 익숙해지기

--- polls/urls.py
+++ polls/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     # views 에서 해당 detail 메소드가 있어야함! 그리고 받은 변수가 해당 메소드에 전달 되어야함!
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/vote/', views.vote, name='vote'),  
+    path('<int:question_id>/result/', views.result, name='result'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render # 화면에 html 을 그려주는 역할
 # from django.http import Http404
 from django.shortcuts import render , get_object_or_404
 from django.urls import reverse
+from django.db.models import F
 
 # def index(request):
 #     return HttpResponse("heelo, wolrld!")
@@ -40,9 +41,17 @@ def vote(request, question_id):
     # 아무것도 안누르고 클릭할 경우 방어 처리 
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    #Choice.DoesNotExist- 서버 데이터와 보여지는 데이터가 안맞을 수 있기 때문에  DoesNotExist로 방어처리 함
+    # case1 ) 선택하는 와중에 선택지가 삭제 되었을 경우 
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {'question': question, 'error_message': '선택이 없습니다.'})
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': f"선택이 없습니다. id={request.POST['choice']}"})
     else:
-        selected_choice.votes += 1
+        # 동시에 2명이 이 기능을 동작하면, 결과가 2가 아닌 1 이 나올수 잇음 
+        # 이럴 경우에는 계산 처리를 서버가 아닌 db 에서 처리하도록 함 
+        selected_choice.votes += F('votes') + 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:index'))
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
+
+def result(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question': question})
