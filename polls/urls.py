5fcc9d8fb0091351768c2e1ec68835c6487a986e
ShinDohee <intei0909@gmail.com>
2024-10-13 11:17:09 +0900

    Django 익숙해지기  - 상세 만들기  - 모델링하기

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('some_url', views.some_url), # / 잘 체킹 할것!
+    # 아래 변수에서 숫자를 받을 경우, <int:question_id> 으로 표현하고 
+    # views 에서 해당 detail 메소드가 있어야함! 그리고 받은 변수가 해당 메소드에 전달 되어야함!
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),  
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,48 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
-from django.shortcuts import render
+from django.shortcuts import render # 화면에 html 을 그려주는 역할 
+# from django.http import Http404
+from django.shortcuts import render , get_object_or_404
+from django.urls import reverse
 
+# def index(request):
+#     return HttpResponse("heelo, wolrld!")
 
 def some_url(request):
     return HttpResponse("some url 구현함")
 
-
+# order_by(컬럼명)  역순으로 정렬 
+# render(request, 'polls/index.html', context) html 에 데이터 그리는 것 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'first_question': latest_question_list[0]}
+    # 16강 
+    # output = '.'.join([q.question_text for q in latest_question_list])   
+    # context = {'first_question': latest_question_list[0]}
+
+    #17강
+    context = {'questions': latest_question_list}
+    #화면에 no Question 확인 용 
+    #context = {'questions': []}
     return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    # object가 없으면 404로 처리하라  
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    # 아무것도 안누르고 클릭할 경우 방어 처리 
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': '선택이 없습니다.'})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:index'))
